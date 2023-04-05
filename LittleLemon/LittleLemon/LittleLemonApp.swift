3bf79a1b5a74fb4928bcfc69c22b0e579fd26811
Kyle Wilson <kyletylerwilson@gmail.com>
2023-04-05 14:48:17 -0400

    first commit

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Kyle Wilson on 2023-04-05.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
