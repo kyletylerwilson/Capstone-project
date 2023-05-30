2a5b021eb21abcc4a04920cbe73a3c12535ed1bb
Kyle Wilson <kyletylerwilson@gmail.com>
2023-05-30 16:01:06 -0400

    commiting step 1

---
+++ LittleLemon/LittleLemon/UserProfile.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfile.swift
+//  LittleLemon
+//
+//  Created by Kyle Wilson on 2023-05-30.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
