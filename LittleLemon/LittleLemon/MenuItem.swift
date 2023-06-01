4b4e169570bcc9d93b50d9995e2f29c7a4563d11
Kyle Wilson <kyletylerwilson@gmail.com>
2023-06-01 15:58:42 -0400

    second step

---
+++ LittleLemon/LittleLemon/MenuItem.swift
@@ -0,0 +1,20 @@
+//
+//  MenuItem.swift
+//  LittleLemon
+//
+//  Created by Kyle Wilson on 2023-05-31.
+//
+
+import SwiftUI
+
+struct MenuItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuItem_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuItem()
+    }
+}
---
+++ LittleLemon/LittleLemon/MenuList.swift
@@ -0,0 +1,20 @@
+//
+//  MenuList.swift
+//  LittleLemon
+//
+//  Created by Kyle Wilson on 2023-05-31.
+//
+
+import SwiftUI
+
+struct MenuList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuList_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuList()
+    }
+}
